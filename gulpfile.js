e8446b8de74249e05cce8f65ace5821112d70133
Fabiola Ampuero <fabi.ampuero.chalco@gmail.com>
2017-09-11 12:49:04 -0500

    add gulp and sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,11 @@ gulp.task('default', function(){
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src('app/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('app/css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('app/scss/**/*.scss', ['sass']);
 });
